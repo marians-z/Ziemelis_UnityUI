88a18b104194bc3d8476f4480e378afb581f1403
marians <marians15150@gmail.com>
2021-03-23 13:30:15 +0200

    Pievienots dropdown

--- Assets/Skripti/paradisana.cs
+++ Assets/Skripti/paradisana.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class paradisana : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject krekls;
+    public GameObject bikses;
+    public GameObject cepures;
+    public GameObject attelsmainas;
+    public GameObject slider1;
+    public GameObject slider2;
+    public Sprite[] masivs;
+    public AudioSource skanasAvots;
+    public AudioClip skana;
+    public void kreklson(bool ok){
+        krekls.SetActive(ok);
     }
+    public void bikseson(bool ok) {
+        bikses.SetActive(ok);
+    }
+    public void cepureon(bool ok) {
+        cepures.SetActive(ok);
+    }
+    public void dropdown(int skaitlis){
+        if(skaitlis == 0)
+        {
+            attelsmainas.GetComponent<Image>().sprite = masivs[0];
+        }
+        else if(skaitlis == 1)
+        {
+            attelsmainas.GetComponent<Image>().sprite = masivs[1];
+        }
+        else if(skaitlis == 2)
+        {
+            attelsmainas.GetComponent<Image>().sprite = masivs[2];
+        }
+    }
+  public void Lielums1(){
+      float vert1=slider1.GetComponent<Slider>().value;
+      attelsmainas.transform.localScale = new Vector2(1F*vert1, 1);
+  }
+  public void Lielums2(){
+      float vert2=slider2.GetComponent<Slider>().value;
+      attelsmainas.transform.localScale = new Vector2(1, 1F*vert2);
+  }
+  public void uzattela(){
+      skanasAvots.PlayOneShot(skana);
+  }
+  public void noattela(){
+      skanasAvots.Stop();
+  }
+        
+ }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
