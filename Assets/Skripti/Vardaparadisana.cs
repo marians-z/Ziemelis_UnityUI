6826344974481b0734bdb74970b0efa36ba960b0
marians <marians15150@gmail.com>
2021-03-23 13:05:18 +0200

    pievienots teksts

---
+++ Assets/Skripti/Vardaparadisana.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Vardaparadisana : MonoBehaviour
+{
+    public string text;
+    public string year;
+    public GameObject input1;
+    public GameObject input2;
+    public GameObject tekstaAttelot;
+    public void saveyear() {
+        text =input1.GetComponent<Text>().text;
+        year =input2.GetComponent<Text>().text;
+        tekstaAttelot.GetComponent<Text>().text=text.ToUpper()+" "+year.ToUpper()+ " gadus vecs";
+
+    }
+
+}
